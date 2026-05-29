d6baa711591062436665cd4ca1f7cc7a2a0707d2
Smriti Walia <151732296+iismrityii@users.noreply.github.com>
2026-05-29 12:45:56 +0530

    Time: 0 ms (100%), Space: 43.2 MB (6.39%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        StringBuilder sb = new StringBuilder(palindrome);
+        int n = palindrome.length();
+        if(n == 1) return "";
+
+        for(int i = 0; i < n / 2; i++) {
+            if(sb.charAt(i) != 'a') {
+                sb.setCharAt(i, 'a');
+                return sb.toString();
+            }
+        }
+
+        sb.setCharAt(n - 1, 'b');
+        return sb.toString();
+    }
+}
