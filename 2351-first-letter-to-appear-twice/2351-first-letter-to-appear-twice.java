18bc6f59325bd94af16b2c2b76da758550899c6e
Smriti Walia <151732296+iismrityii@users.noreply.github.com>
2026-06-18 23:07:46 +0530

    Time: 0 ms (100%), Space: 42.9 MB (51.22%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashSet<Character> hs = new HashSet<>();
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(hs.contains(ch)) {
+                return ch;
+            }
+
+            hs.add(ch);
+        }
+
+        return ' ';
+    }
+}
