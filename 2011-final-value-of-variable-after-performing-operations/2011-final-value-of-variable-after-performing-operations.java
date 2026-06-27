22c106d253e4e0dfacd009343a875550eb9ecb0f
Smriti Walia <151732296+iismrityii@users.noreply.github.com>
2026-06-28 01:54:00 +0530

    Time: 1 ms (74.2%), Space: 44.5 MB (28.72%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int res = 0;
+
+        for(String op : operations) {
+            if(op.equals("++X") || op.equals("X++")) {
+                res++;
+            }
+            else {
+                res--;
+            }
+        }
+
+        return res;
+    }
+}
