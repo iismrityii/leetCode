c5da557789b6a87f418a16688c061f591b6517e0
Smriti Walia <151732296+iismrityii@users.noreply.github.com>
2026-06-30 12:55:43 +0530

    Time: 3 ms (93.75%), Space: 44.3 MB (11.03%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRabbits(int[] answers) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int num : answers) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+
+        int t = 0;
+        for(int ans : hm.keySet()) {
+            int freq = hm.get(ans);
+            int groupSize = ans + 1;
+            int grps = (freq + groupSize - 1) / groupSize;
+
+            t += grps * groupSize;
+        }
+
+        return t;
+    }
+}
