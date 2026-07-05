b71a54d9c584a9632063fa1fd5a3bb3044d3825a
Smriti Walia <151732296+iismrityii@users.noreply.github.com>
2026-07-05 16:26:40 +0530

    Time: 0 ms (100%), Space: 42 MB (69.6%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,19 @@
 class Solution {
-    public int fib(int n) {
-        if(n == 0 || n == 1) return n;
+    private int solve(int n, int dp[]) {
+        if(n == 0 || n == 1) {
+            return n;
+        }
 
-        return fib(n - 1) + fib(n - 2);
+        if(dp[n] != 0) {
+            return dp[n];
+        }
+
+        dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        return dp[n];
+    }
+
+    public int fib(int n) {
+        int dp[] = new int[n + 1];
+        return solve(n, dp);
     }
 }
