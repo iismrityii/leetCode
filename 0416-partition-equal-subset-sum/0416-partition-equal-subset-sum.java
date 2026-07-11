d247ddac48b490342a3359da145f16df14b46178
Smriti Walia <151732296+iismrityii@users.noreply.github.com>
2026-07-11 16:40:09 +0530

    Time: 50 ms (47.15%), Space: 47.3 MB (60.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,34 +1,8 @@
 class Solution {
-    Boolean dp[][];
-    private boolean solve(int i, int currSum, int tar, int nums[]) {
-        if(currSum == tar) {
-            return true;
-        }
-
-        if(i == nums.length) {
-            return false;
-        }
-
-        if(dp[i][currSum] != null) {
-            return dp[i][currSum];
-        }
-
-        boolean pick = false;
-
-        if(currSum + nums[i] <= tar) {
-            pick = solve(i + 1, currSum + nums[i], tar, nums);
-        }
-
-        boolean notPick = solve(i + 1, currSum, tar, nums);
-        
-        dp[i][currSum] = pick || notPick;
-        return dp[i][currSum];
-    }
-
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int i = 0; i < nums.length; i++) {
-            sum += nums[i];
+        for(int num : nums) {
+            sum += num;
         }
 
         if(sum % 2 != 0) {
@@ -36,7 +10,23 @@ class Solution {
         }
 
         int tar = sum / 2;
-        dp = new Boolean[nums.length][tar + 1];
-        return solve(0, 0, tar, nums);
+        int n = nums.length;
+
+        boolean dp[][] = new boolean[n + 1][tar + 1];
+        dp[n][tar] = true;
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int currSum = tar; currSum >= 0; currSum--) {
+                boolean pick = false;
+                if(currSum + nums[i] <= tar) {
+                    pick = dp[i + 1][currSum + nums[i]];
+                }
+
+                boolean notPick = dp[i + 1][currSum];
+                dp[i][currSum] = pick || notPick;
+            }
+        }
+
+        return dp[0][0];
     }
 }
