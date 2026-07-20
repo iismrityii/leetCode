1e0c3080858711c2cbb963389af1ed15162e7de8
Smriti Walia <151732296+iismrityii@users.noreply.github.com>
2026-07-20 17:57:05 +0530

    Time: 33 ms (76.14%), Space: 63.7 MB (56.55%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,37 +1,19 @@
 class Solution {
-    int dp[][];
-    int n;
-    private int solve(int i, int j, String s) {
-        if(i == j) {
-            return 1;
-        }
-
-        if(i > j) {
-            return 0;
-        }
-
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int pick = 0;
-        if(s.charAt(i) == s.charAt(j)) {
-            pick = 2 + solve(i + 1, j - 1, s);
-        }
-
-        int notPick = Math.max(solve(i + 1, j, s), solve(i, j - 1, s));
-
-        dp[i][j] = Math.max(pick, notPick);
-        return dp[i][j];
-    }
-
     public int longestPalindromeSubseq(String s) {
-        this.n = s.length();
-        dp = new int[n][n];
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+        int n = s.length();
+        int dp[][] = new int[n + 1][n + 1];
+
+        for(int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for(int j = i + 1; j < n; j++) {
+                if(s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
         }
 
-        return solve(0, s.length() - 1, s);
+        return dp[0][n - 1];
     }
 }
