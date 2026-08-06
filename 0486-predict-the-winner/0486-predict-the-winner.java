7f8fe08e3d29f917505a975716b982f4278d7738
Smriti Walia <151732296+iismrityii@users.noreply.github.com>
2026-08-06 18:11:23 +0530

    Time: 1 ms (34.65%), Space: 42.8 MB (54.48%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,17 +1,27 @@
 class Solution {
+    int dp[][];
     private int solve(int nums[], int i, int j) {
         if(i == j) {
             return nums[i];
         }
 
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
         int left = nums[i] - solve(nums, i + 1, j);
         int right = nums[j] - solve(nums, i, j - 1);
 
-        return Math.max(left, right);
+        return dp[i][j] = Math.max(left, right);
     }
 
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
+        dp = new int[n][n + 1];
+        for(int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
         int diff = solve(nums, 0, n - 1);
 
         if(diff >= 0) {
