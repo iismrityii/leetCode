226da5bb050afe43c10c24cc2052e451406a4212
Smriti Walia <151732296+iismrityii@users.noreply.github.com>
2026-08-06 16:49:07 +0530

    Time: 33 ms (16.07%), Space: 47.5 MB (9.62%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,11 +1,12 @@
 class Solution {
+    int n;
     int dp[][];
-    private int solve(int i, int remAmt, int coins[]) {
-        if (remAmt == 0) {
+    private int solve(int coins[], int i, int remAmt) {
+        if(remAmt == 0) {
             return 0;
         }
 
-        if (i == coins.length) {
+        if(i == n) {
             return Integer.MAX_VALUE / 2;
         }
 
@@ -15,22 +16,21 @@ class Solution {
 
         int pick = Integer.MAX_VALUE;
         if(remAmt >= coins[i]) {
-            pick = 1 + solve(i, remAmt - coins[i], coins);
+            pick = 1 + solve(coins, i, remAmt - coins[i]);
         }
-
-        int notPick = solve(i + 1, remAmt, coins);
-        dp[i][remAmt] = Math.min(pick, notPick);
-        return dp[i][remAmt];
+        
+        int notPick = solve(coins, i + 1, remAmt);
+        return dp[i][remAmt] = Math.min(pick, notPick);
     }
 
     public int coinChange(int[] coins, int amount) {
-        dp = new int[coins.length][amount + 1];
-        for(int i = 0; i < coins.length; i++) {
+        this.n = coins.length;
+        dp = new int[n][amount + 1];
+        for(int i = 0; i < n; i++) {
             Arrays.fill(dp[i], -1);
         }
 
-        int coin = solve(0, amount, coins);
-        return coin == Integer.MAX_VALUE / 2 ? -1 : coin;
-
+        int ans = solve(coins, 0, amount);
+        return ans >= Integer.MAX_VALUE / 2 ? -1 : ans;
     }
 }
