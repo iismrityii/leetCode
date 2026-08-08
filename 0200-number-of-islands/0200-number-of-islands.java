f8380e454b199baa048d2245ca5c6bc5dcb6a872
Smriti Walia <151732296+iismrityii@users.noreply.github.com>
2026-08-08 19:59:53 +0530

    Time: 2 ms (99.85%), Space: 52.5 MB (33.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,55 +1,29 @@
-class Pair {
-    int r;
-    int c;
-
-    Pair(int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
-}
-
 class Solution {
+    int m;
+    int n;
+    private void dfs(char[][] grid, int i, int j) {
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+    }
+
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean vis[][] = new boolean[m][n];
+        this.m = grid.length;
+        this.n = grid[0].length;
+
         int count = 0;
-
-        Queue<Pair> q = new LinkedList<>();
-
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] == '1' && !vis[i][j]) {
-                    q.offer(new Pair(i, j));
-                    vis[i][j] = true;
+                if(grid[i][j] == '1') {
                     count++;
-                }
-                
-                while(!q.isEmpty()) {
-                    Pair curr = q.poll();
-
-                    int r = curr.r;
-                    int c = curr.c;
-
-                    if(r >= 0 && r < m && c + 1 >= 0 && c + 1 < n && grid[r][c + 1] == '1' && !vis[r][c + 1]) {
-                        vis[r][c + 1] = true;
-                        q.offer(new Pair(r, c + 1));
-                    }
-
-                    if(r + 1 >= 0 && r + 1 < m && c >= 0 && c < n && grid[r + 1][c] == '1' && !vis[r + 1][c]) {
-                        vis[r + 1][c] = true;
-                        q.offer(new Pair(r + 1, c));
-                    }
-
-                    if(r >= 0 && r < m && c - 1 >= 0 && c - 1 < n && grid[r][c - 1] == '1' && !vis[r][c - 1]) {
-                        vis[r][c - 1] = true;
-                        q.offer(new Pair(r, c - 1));
-                    }
-
-                    if(r - 1 >= 0 && r - 1 < m && c >= 0 && c < n && grid[r - 1][c] == '1' && !vis[r - 1][c]) {
-                        vis[r - 1][c] = true;
-                        q.offer(new Pair(r - 1, c));
-                    }
+                    dfs(grid, i, j);
                 }
             }
         }
